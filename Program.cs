f162cf52f99d5a0b74498b40915d1f9ecaa383ec
isel06002 <isel06002@E-PW0234HX>
2023-10-04 10:49:40 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Inlämning_3.__3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många timmar vill du hyra bil");
+            int timmar = int.Parse(Console.ReadLine());
+            int kostnad = 80 * timmar;
+            if (kostnad > 950)
+            {
+                kostnad = 950;
+            }
+            Console.WriteLine("Detta kommer att kosta "+kostnad+" kr.");
+            Console.ReadKey();
+        }
+    }
+}
